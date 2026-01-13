9b2ac3515a51372d7b31fdc336dcca900a4c2474
ddobroiu <d_dobroiu@YAHOO.COM>
2026-01-13 08:38:46 +0200

    000

---
+++ global.d.ts
@@ -0,0 +1,20 @@
+/// <reference types="react" />
+
+declare namespace JSX {
+    interface IntrinsicElements {
+        'model-viewer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+            src?: string;
+            poster?: string;
+            alt?: string;
+            'shadow-intensity'?: string;
+            'camera-controls'?: boolean;
+            'auto-rotate'?: boolean;
+            ar?: boolean;
+            'ar-modes'?: string;
+            'tone-mapping'?: string;
+            scale?: string;
+            orientation?: string;
+            ref?: any;
+        }, HTMLElement>;
+    }
+}
